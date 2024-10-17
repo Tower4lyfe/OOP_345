b2b32fff2927c39cb131b3f1565e4030d928df3c
Tower4lyfe <ttlau3@myseneca.ca>
2024-10-17 12:58:12 -0400

    Changes to E2

---
+++ Excerciese/Excercise_2/TimedTask.cpp
@@ -0,0 +1,18 @@
+#include "TimedTask.h"
+
+void seneca::TimedTask::startClock()
+{
+}
+
+void seneca::TimedTask::stopClock()
+{
+}
+
+void seneca::TimedTask::addTask(char *str)
+{
+}
+
+std::ostream &seneca::operator<<(std::ostream &os, TimedTask obj)
+{
+    // TODO: insert return statement here
+}
---
+++ Excerciese/Excercise_2/TimedTask.h
@@ -0,0 +1,36 @@
+#ifndef SENECA_TIMEDTASK_H
+#define SENECA_TIMEDTASK_H
+
+#include <iostream>
+
+namespace seneca
+{
+    constexpr size_t Max = 10;
+    class TimedTask
+    {
+        int numberOfRecords{};
+        std::chrono::time_point<std::chrono::system_clock> start {};
+        std::chrono::time_point<std::chrono::system_clock> end{};
+        struct task 
+        {
+            std::string task_name{};
+            std::string time_unit{};
+            std::chrono::steady_clock::duration duration;
+        } tasks[Max];
+
+        public:
+            TimedTask(){};
+            ~TimedTask();
+            TimedTask(TimedTask& copy);
+            TimedTask& operator = (TimedTask& Copy);
+            void startClock();
+            void stopClock();
+            void addTask(char* str);
+            friend std::ostream& operator <<(std::ostream& os, TimedTask obj);
+    };
+
+
+}
+
+
+#endif
