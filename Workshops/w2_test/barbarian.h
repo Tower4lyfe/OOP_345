8b7901da88cb9bd803fc0359a793668df6d1b228
To Tsz Lau <totszlau@gmail.com>
2024-10-20 21:32:10 -0400

    lets see

--- Workshops/w2_test/barbarian.h
+++ Workshops/w2_test/barbarian.h
@@ -1,63 +1,70 @@
 #ifndef SENECA_BARBARIAN_H
 #define SENECA_BARBARIAN_H
-
+#include "character.h"
 #include "characterTpl.h"
-#include "weapons.h"
-#include "abilities.h"
 
-namespace seneca
+namespace seneca 
 {
-    template <typename T, typename Ability_t, typename Weapon_t>
+    template<typename T, typename Ability_t, typename Weapon_t>
     class Barbarian : public CharacterTpl<T>
     {
-    private:
-        Ability_t m_ability;
-        Weapon_t m_weapons[2];
         int m_baseAttack;
-        int m_baseDefense;
+        int m_baseDefense;    
+        Ability_t m_ability;
+        Weapon_t m_weapon[2];
 
-    public:
-        Barbarian(const char* name, int healthMax, int baseAttack, int baseDefense, Weapon_t primaryWeapon, Weapon_t secondaryWeapon)
-            : CharacterTpl<T>(name, healthMax), m_baseAttack(baseAttack), m_baseDefense(baseDefense)
+        public:
+        //constructor
+        Barbarian(const char* name, int healthMax, int baseAttack, int baseDefense, Weapon_t primaryWeapon, Weapon_t secondaryWeapon):
+        CharacterTpl<T>(name, healthMax), m_baseAttack(baseAttack), m_baseDefense(baseDefense),m_weapon{primaryWeapon, secondaryWeapon}{};
+
+        int getAttackAmnt()const
         {
-            m_weapons[0] = primaryWeapon;
-            m_weapons[1] = secondaryWeapon;
+            return m_baseAttack + static_cast<double>(m_weapon[0])/2 + static_cast<double>(m_weapon[1])/2;
         }
 
-        int getAttackAmnt() const override
+        int getDefenseAmnt() const{return m_baseDefense;}
+
+        //returning a new object base on this object
+        Character* clone() const
         {
-            return m_baseAttack + static_cast<int>(m_weapons[0]) / 2 + static_cast<int>(m_weapons[1]) / 2;
+            return new Barbarian<T, Ability_t, Weapon_t>(*this);
         }
-
-        int getDefenseAmnt() const override { return m_baseDefense; }
-
-        Character* clone() const override { return new Barbarian<T, Ability_t, Weapon_t>(*this); }
-
-        void attack(Character* enemy) override
+        
+        //ATTTAAAAACK!!
+        void attack(Character* enemy)
         {
             std::cout << this->getName() << " is attacking " << enemy->getName() << ".\n";
-            m_ability.useAbility(this);  // Use ability to activate beneficial effects
+            //ACTIVATING ME SUPA POWER
+            m_ability.useAbility(this);
 
-            int total_attack = getAttackAmnt();
-            m_ability.transformDamageDealt(total_attack);  // Enhance damage with ability effects
+            int atk_dmg = getAttackAmnt();
+            m_ability.transformDamageDealt(atk_dmg);
 
-            std::cout << "Barbarian deals " << total_attack << " melee damage!\n";
-            enemy->takeDamage(total_attack);
+            std::cout << "    Barbarian deals " << atk_dmg << " melee damage!\n";
+            enemy->takeDamage(atk_dmg);
         }
 
-        void takeDamage(int dmg) override
-{
-    std::cout << this->getName() << " is attacked for " << dmg << " damage.\n";
-    std::cout << "Barbarian has a defense of " << m_baseDefense << ". Reducing damage received.\n";
-    // Apply ability transformations to damage received
-    int reduced_dmg = dmg -m_baseDefense;
-    m_ability.transformDamageReceived(reduced_dmg);
+        //OHNO, I'VE BEEN HIT 
+        void takeDamage(int dmg) 
+        {
+            std::cout << this->getName() << " is attacked for " << dmg << " damage.\n";
+            std::cout << "    Barbarian has a defense of " << m_baseDefense << ". Reducing damage received.\n";
 
-    CharacterTpl<T>::takeDamage(reduced_dmg);  // The reduction is handled by CharacterTpl
-}
+            //Cooler If statement
+            int reduced_dmg{};
+            dmg - m_baseAttack < 0? reduced_dmg = 0: reduced_dmg = dmg -m_baseDefense;
+            //almost forgot
+            m_ability.transformDamageReceived(reduced_dmg);
 
+            //need to specify
+            CharacterTpl<T>::takeDamage(reduced_dmg);
+
+        }
 
     };
+
 }
+
 
 #endif
