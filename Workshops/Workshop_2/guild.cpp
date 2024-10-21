156a44f64560086e7b98f9e92c33e020c13c71f3
To Tsz Lau <totszlau@gmail.com>
2024-10-20 21:51:46 -0400

    Fixing guild

--- Workshops/Workshop_2/guild.cpp
+++ Workshops/Workshop_2/guild.cpp
@@ -1,173 +1,164 @@
-#include "guild.h"
+// Guild.cpp
+#include "Guild.h"
+#include <iostream>
 
-//BASICALLY JUST COPY PASTING TEAM MODULE
 namespace seneca
 {
-    //RULE OF 5
-        Guild::~Guild()
+    // Constructor
+    Guild::Guild(const std::string& name) : m_name(name), m_memberCount(0)
+    {
+        m_members = new Character*[1];
+    }
+
+    // Destructor
+    Guild::~Guild()
+    {
+        for (size_t i = 0; i < m_memberCount; ++i)
+        {
+            delete m_members[i];
+        }
+        delete[] m_members;
+    }
+
+    // Copy Constructor
+    Guild::Guild(const Guild& other) : m_name(other.m_name), m_memberCount(other.m_memberCount)
+    {
+        m_members = new Character*[m_memberCount];
+        for (size_t i = 0; i < m_memberCount; ++i)
+        {
+            m_members[i] = other.m_members[i]->clone();
+        }
+    }
+
+    // Copy Assignment Operator
+    Guild& Guild::operator=(const Guild& other)
+    {
+        if (this != &other)
         {
             for (size_t i = 0; i < m_memberCount; ++i)
             {
                 delete m_members[i];
-                m_members[i] = nullptr;
             }
             delete[] m_members;
-            m_members = nullptr;
-            m_memberCount = 0;
-        }
 
-        Guild::Guild(const Guild&other)
-        : m_name(other.m_name), m_memberCount(other.m_memberCount)
-        {
+            m_name = other.m_name;
+            m_memberCount = other.m_memberCount;
             m_members = new Character*[m_memberCount];
             for (size_t i = 0; i < m_memberCount; ++i)
             {
                 m_members[i] = other.m_members[i]->clone();
             }
         }
+        return *this;
+    }
 
-        Guild& Guild::operator=(const Guild&other)
+    // Move Constructor
+    Guild::Guild(Guild&& other) noexcept : m_name(std::move(other.m_name)), m_members(other.m_members), m_memberCount(other.m_memberCount)
+    {
+        other.m_members = nullptr;
+        other.m_memberCount = 0;
+    }
+
+    // Move Assignment Operator
+    Guild& Guild::operator=(Guild&& other) noexcept
+    {
+        if (this != &other)
         {
-            if (this != &other)
+            for (size_t i = 0; i < m_memberCount; ++i)
             {
-                for (size_t i = 0; i < m_memberCount; ++i)
-                {
-                    delete m_members[i];
-                }
-                delete[] m_members;
-
-                m_name = other.m_name;
-                m_memberCount = other.m_memberCount;
-                m_members = new Character*[m_memberCount];
-                for (size_t i = 0; i < m_memberCount; ++i)
-                {
-                    m_members[i] = other.m_members[i]->clone();
-                }
+                delete m_members[i];
             }
-            return *this;
-        }
+            delete[] m_members;
 
-        Guild::Guild(Guild&&other)noexcept
-        : m_name(std::move(other.m_name)), m_members(other.m_members), m_memberCount(other.m_memberCount)
-        {
+            m_name = std::move(other.m_name);
+            m_members = other.m_members;
+            m_memberCount = other.m_memberCount;
+
             other.m_members = nullptr;
             other.m_memberCount = 0;
         }
+        return *this;
+    }
 
-        Guild& Guild::operator=(Guild&&other)noexcept
+    // Add Member
+    void Guild::addMember(Character* character)
+    {
+        for (size_t i = 0; i < m_memberCount; ++i)
         {
-            if (this != &other)
-            {
-                for (size_t i = 0; i < m_memberCount; ++i)
-                {
-                    delete m_members[i];
-                }
-                delete[] m_members;
-
-                m_name = std::move(other.m_name);
-                m_members = other.m_members;
-                m_memberCount = other.m_memberCount;
-
-                other.m_members = nullptr;
-                other.m_memberCount = 0;
-            }
-            return *this;
+            if (m_members[i]->getName() == character->getName())
+                return;
         }
 
-    //Not Rule of 5
-
-        //NOT CONST CHARACTER
-        void Guild::addMember(Character* c)
+        Character** newMembers = new Character*[m_memberCount + 1];
+        for (size_t i = 0; i < m_memberCount; ++i)
         {
+            newMembers[i] = m_members[i];
+        }
+        delete[] m_members;
+        m_members = newMembers;
+
+        int newHealthMax = character->getHealthMax() + 300;
+        character->setHealthMax(newHealthMax);
+
+        m_members[m_memberCount++] = character->clone();
+    }
+
+    // Remove Member
+    void Guild::removeMember(const std::string& name)
+    {
+        for (size_t i = 0; i < m_memberCount; ++i)
+        {
+            if (m_members[i]->getName() == name)
+            {
+                delete m_members[i];
+
+                for (size_t j = i; j < m_memberCount - 1; ++j)
+                {
+                    m_members[j] = m_members[j + 1];
+                }
+                --m_memberCount;
+
+                if (m_memberCount > 0)
+                {
+                    Character** newMembers = new Character*[m_memberCount];
+                    for (size_t k = 0; k < m_memberCount; ++k)
+                    {
+                        newMembers[k] = m_members[k];
+                    }
+                    delete[] m_members;
+                    m_members = newMembers;
+                }
+                else
+                {
+                    delete[] m_members;
+                    m_members = nullptr;
+                }
+
+                break;
+            }
+        }
+    }
+
+    // Access Operator
+    Character* Guild::operator[](size_t index) const
+    {
+        if (index < m_memberCount)
+            return m_members[index];
+        return nullptr;
+    }
+
+    // Show Members
+    void Guild::showMembers() const
+    {
+        if (m_memberCount == 0)
+            std::cout << "No Guild.\n";
+        else
+        {
+            std::cout << "[Guild] " << m_name << "\n";
             for (size_t i = 0; i < m_memberCount; ++i)
             {
-                if (m_members[i]->getName() == c->getName())
-                    return; // Prevent adding duplicate characters by name
-            }
-
-            // Resize if necessary
-            Character** newMembers = new Character*[m_memberCount + 1];
-            for (size_t i = 0; i < m_memberCount; ++i)
-            {
-                newMembers[i] = m_members[i];
-            }
-            delete[] m_members;
-            m_members = newMembers;
-
-            // Increase maximum health by 300
-            int newHealthMax = c->getHealthMax() + 300;
-            c->setHealthMax(newHealthMax);
-
-            m_members[m_memberCount++] = c;
-        }
-
-
-        //this part is annoying... careful, don't delete the actual person
-        void Guild::removeMember(const std::string&c)
-        {
-            for (size_t i = 0; i < m_memberCount; ++i)
-            {
-                if (m_members[i]->getName() == c)
-                {
-                    // Decrease maximum health by 300
-                    int reducedHealthMax = (m_members[i]->getHealthMax() > 300) ? (m_members[i]->getHealthMax() - 300) : 0;
-                    m_members[i]->setHealthMax(reducedHealthMax);
-
-                    // Shift the remaining members to fill the gap
-                    for (size_t j = i; j < m_memberCount - 1; ++j)
-                    {
-                        m_members[j] = m_members[j + 1];
-                    }
-                    --m_memberCount;
-
-                    // Resize the array
-                    if (m_memberCount > 0)
-                    {
-                        Character** newMembers = new Character*[m_memberCount];
-                        for (size_t k = 0; k < m_memberCount; ++k)
-                        {
-                            newMembers[k] = m_members[k];
-                        }
-                        delete[] m_members;
-                        m_members = newMembers;
-                    }
-                    else
-                    {
-                        delete[] m_members;
-                        m_members = nullptr;
-                    }
-
-                    break;
-                }
+                std::cout << *m_members[i] << std::endl;
             }
         }
-
-
-        Character* Guild::operator[](size_t idx)const
-        {
-            if (idx < m_memberCount)
-                return m_members[idx];
-            return nullptr;
-        }
-
-
-        void Guild::showMembers()const
-        {
-            if(m_memberCount == 0)
-                std::cout << "No Guild.\n";
-            else
-            {
-                std::cout << "[Guild] " << m_name << "\n";
-                for(size_t i = 0; i < m_memberCount; ++i)
-                {
-                    std::cout << *m_members[i] << std::endl;
-                }
-            }
-        }
-
-
-
-
-
-
+    }
 }
--- Workshops/Workshop_2/guild.h
+++ Workshops/Workshop_2/guild.h
@@ -1,33 +1,31 @@
 #ifndef SENECA_GUILD_H
 #define SENECA_GUILD_H
+
 #include "character.h"
-
-
-
+#include <string>
 
 namespace seneca
 {
     class Guild
     {
-        std::string m_name; //wasn't sure cstring or string, so I went with string 
+    private:
+        std::string m_name;
         Character** m_members;
-        size_t m_memberCount; 
+        size_t m_memberCount;
 
-        public:
-        Guild(const std::string& name) : m_name(name), m_members(nullptr), m_memberCount(0) {m_members = new Character*[1];};
-        //RULE OF 5
+    public:
+        Guild(const std::string& name);
+        //rule of 5
         ~Guild();
-        Guild(const Guild&other);
-        Guild& operator=(const Guild&other);
-        Guild(Guild&&other)noexcept;
-        Guild& operator=(Guild&&other)noexcept;
-
-        //Not Rule of 5
-        void addMember(Character* c);//NOT CONST
-        void removeMember(const std::string&c);
-        Character* operator[](size_t idx)const;
-        void showMembers()const;
-
+        Guild(const Guild& other);
+        Guild& operator=(const Guild& other);
+        Guild(Guild&& other) noexcept;
+        Guild& operator=(Guild&& other) noexcept;
+        //not rule of 5
+        void addMember(Character* character);
+        void removeMember(const std::string& name);
+        Character* operator[](size_t index) const;
+        void showMembers() const;
     };
 }
 
