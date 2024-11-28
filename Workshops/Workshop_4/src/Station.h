0fe13e4aec7a5fba6c192c0c1ab7742bf3131412
To Tsz Lau <totszlau@gmail.com>
2024-11-28 01:57:32 -0500

    Finish station module design

--- Workshops/Workshop_4/src/Station.h
+++ Workshops/Workshop_4/src/Station.h
@@ -10,7 +10,7 @@ namespace seneca
         std::string m_name;
         std::string m_description;
         unsigned int m_serialNumber;
-        size_t m_numberOfItems;
+        size_t m_quantity;
 
         static int m_widthField;
         static int id_generator;
@@ -19,7 +19,11 @@ namespace seneca
         Station() = default;
         ~Station()= default;
         Station(const std::string&);
-
+        const std::string& getItemName() const;
+        size_t getNextSerialNumber();
+        size_t getQuantity() const;
+        void updateQuantity();
+        void display(std::ostream&os, bool full)const;
     };
 }
 
