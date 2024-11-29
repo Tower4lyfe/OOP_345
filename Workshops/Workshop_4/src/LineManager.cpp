e26b709f614e45cf6cb33e58aebfc60261c1e4fc
To Tsz Lau <totszlau@gmail.com>
2024-11-29 15:02:39 -0500

    Finish LineManager header file design, and the implementation of the custom constructor

--- Workshops/Workshop_4/src/LineManager.cpp
+++ Workshops/Workshop_4/src/LineManager.cpp
@@ -1,1 +1,90 @@
 #include "lineManager.h"
+#include "Workstation.h"
+#include <algorithm>
+
+
+
+namespace seneca 
+{
+    LineManager::LineManager(const std::string& file, const std::vector<Workstation*>& stations)
+    {
+        if (file.empty() == true) 
+        {
+		    throw std::string("ERROR: No filename provided.");
+	    }
+
+	    std::ifstream fileObj(file);
+
+	    if (!fileObj)
+		    throw std::string("Unable to open [") + file + "] file.";
+
+
+
+        Utilities util;
+        util.setDelimiter('|');
+        std::vector<std::string> nextStationNames;
+
+        std::string line;
+	    while (!fileObj.eof())
+	    {
+            
+            size_t next_pos = 0;
+            bool more = false;
+		    std::getline(fileObj, line);
+
+            std::string currentStationName = util.extractToken(line, next_pos, more);
+            std::string nextStationName = more? util.extractToken(line, next_pos, more) : "";
+
+            //find if to find station that fits the name
+            auto currentStation = std::find_if(stations.begin(), stations.end(), 
+            [&currentStationName](Workstation* station)
+            {
+                return station->getItemName() == currentStationName;
+            });
+
+            if(currentStation == stations.end()){throw std::string{"No Station Found.\n"};}
+
+            //there is a next station
+            if(!nextStationName.empty())
+            {
+                auto nextStation = std::find_if(stations.begin(), stations.end(), 
+                [&nextStationName](Workstation* station)
+                {
+                    return station->getItemName() == nextStationName;
+                });
+
+                if(nextStation == stations.end())
+                    throw std::string{"No next Station with that name.\n"};
+
+                (*currentStation)->setNextStation(*nextStation);
+                nextStationNames.push_back(nextStationName);
+            }else
+            {
+                //just be safe.
+                (*currentStation)->setNextStation(nullptr);
+            }
+            m_activeLine.push_back(*currentStation);
+	    }
+
+	    fileObj.close();
+
+            // Find the first station
+        m_firstStation = *std::find_if(m_activeLine.begin(), m_activeLine.end(),
+                [&nextStationNames](Workstation *station) 
+                {
+                    //If its name does not exist in the nextStaionNames
+                    return std::none_of(nextStationNames.begin(),nextStationNames.end(),
+                            [&station](const std::string &nextName) 
+                            {
+                                return station->getItemName() == nextName;
+                            });
+                });
+
+        if (m_firstStation == nullptr) 
+        {
+            throw std::string("Could not identify the first station.");
+        }
+
+        m_cntCustomerOrder = g_pending.size();
+    }
+}
--- Workshops/Workshop_4/src/LineManager.h
+++ Workshops/Workshop_4/src/LineManager.h
@@ -1,9 +1,26 @@
 #ifndef SENECA_LINEMANAGER_H
 #define SENECA_LINEMANAGER_H
+#include "Workstation.h"
+#include <vector>
+#include <fstream>
+#include <string>
 
 namespace seneca
 {
-    class LineManager{};
+    class LineManager
+    {
+        std::vector<Workstation*> m_activeLine;
+        size_t m_cntCustomerOrder;
+        Workstation* m_firstStation;
+
+        public:
+        LineManager() = default;
+        ~LineManager() = default;
+        LineManager(const std::string& file, const std::vector<Workstation*>& stations);
+        void reorderStations();
+        bool run(std::ostream& os);
+        void display(std::ostream& os) const;
+    };
 }
 
 #endif
